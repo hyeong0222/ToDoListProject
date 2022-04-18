961b2348473260fcadcd7deffdc45d106f15c13a
hyeong0222 <hyeong0222@gmail.com>
2022-04-18 16:49:08 +0900

    Initializing threeten in the main application class so it can be used throughout the app

--- app/src/main/java/com/example/todolistproject/application/MainApplication.kt
+++ app/src/main/java/com/example/todolistproject/application/MainApplication.kt
@@ -1,7 +1,13 @@
 package com.example.todolistproject.application
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
